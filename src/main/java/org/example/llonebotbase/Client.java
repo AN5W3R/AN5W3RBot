283cb7aa39386d9d03ee298df4f23f351081b608
AN5W3R <2044284028@qq.com>
2024-07-16 16:05:27 +0800

    分层并完成TODO
    TODO:
    更换AI API
    Service层

--- src/main/java/org/example/llonebotbase/Client.java
+++ src/main/java/org/example/llonebotbase/Client.java
@@ -2,13 +2,14 @@ package org.example.llonebotbase;
 
 import com.alibaba.fastjson.JSONObject;
 import jakarta.websocket.*;
+import org.example.llonebotbase.dao.MsgDao;
 import org.example.llonebotbase.entity.Message;
 import org.example.llonebotbase.entity.MsgItem;
 import org.example.llonebotbase.entity.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -54,93 +55,57 @@ public class Client {
         logger.info("连接错误！");
     }
 
-    //
     @OnMessage
-    public void onMessage(String message) throws UnsupportedEncodingException {//message对象正在以json字符串的形式传输,这里要重构,让它以对象形式传输
-        System.out.println("\n"+message);
-        if (message.contains("\"post_type\":\"message\"")) {//信息类型为消息
+    public void onMessage(String message) throws IOException {
+//        System.out.println("\n"+message);
+        if (message.contains("\"post_type\":\"message\"")) {//处理message信息
             Message parseObject = JSONObject.parseObject(message, Message.class);//JSON转换为对象
 
             if("private".equals(parseObject.getMessageType())){//私聊信息
-                sendPrivateMsg(parseObject);
+                sendMsg(parseObject, parseObject.getMessageType());
             }
             if ("group".equals(parseObject.getMessageType())) {//群聊信息
                 if(parseObject.getRawMessage().contains("[CQ:at,qq="+parseObject.getSelfId()+"]")){//被@的情况message
-                    sendGroupMsg(parseObject);
+                    sendMsg(parseObject, parseObject.getMessageType());
                 }
             }
         }
     }
     /**
-     * 好友消息
+     * 消息
      */
-    //TODO 私聊和群聊合并一个函数 会话层
-    public synchronized static void sendPrivateMsg(Message parseObject) throws UnsupportedEncodingException {
+    public synchronized static void sendMsg(Message parseObject,String detailType) throws IOException {
         String message = parseObject.getRawMessage();
-        logger.info("收到好友" + parseObject.getUserId() + "的消息：" + message);
 
+        Map<String, Object> params = new HashMap<>();
+        params.put("detail_type",detailType);
+
+        if("private".equals(detailType)){
+            logger.info("收到好友" + parseObject.getUserId() + "的消息：" + message);
+            params.put("user_id", parseObject.getUserId());
+        } else if("group".equals(detailType)){
+            logger.info("收到群" + parseObject.getGroupId() + "的消息：" + message);
+            params.put("group_id", parseObject.getGroupId());//设置要发送的群
+        } else {
+            return;
+        }
+
+//TODO        获取msgList的逻辑可以写在service
+        ArrayList<MsgItem> msgList = new ArrayList<>();
+        MsgItem item = new MsgItem("text","text",MsgDao.getTextByMsg(message));
+        msgList.add(item);
+//        MsgItem item1 = new MsgItem("image","file","https://gchat.qpic.cn/gchatpic_new/1542338612/758025242-3108042462-E04E0D37BB15FF418729C4AE9118A180/0?term=255&is_origin=0");
+//        msgList.add(item1);
+
+        params.put("message",msgList);
         Request<Object> paramsRequest = new Request<>();
         paramsRequest.setAction("send_msg");
-        Map<String, Object> params = new HashMap<>();
-        params.put("user_id", parseObject.getUserId());
-        params.put("detail_type","private");
-
-        String resMsg = LocalChat.ChatByMsg(message);
-        if (resMsg == null) {
-            resMsg = "出了点小问题...";
-        }
-        params.put("message",resMsg);
         paramsRequest.setParams(params);
-        logger.info("收到好友" + parseObject.getUserId() + "的消息：" + message);
 
         String msg = JSONObject.toJSONString(paramsRequest);//将请求转换为json
         instance.session.getAsyncRemote().sendText(msg);//发出请求
     }
 
-    /**
-     * 群聊信息
-     */
-    public synchronized static void sendGroupMsg( Message parseObject) throws UnsupportedEncodingException {
-        String message = parseObject.getRawMessage();
-        logger.info("收到群" + parseObject.getGroupId() + "的消息：" + message);
-
-        Request<Object> paramsRequest = new Request<>();//用于发送请求的对象
-        paramsRequest.setAction("send_msg");//设置发送消息
-        Map<String, Object> params = new HashMap<>();
-        params.put("group_id", parseObject.getGroupId());//设置要发送的群
-        params.put("detail_type","group");
-
-
-//        params.put("message",resMsg);//应该放进data
-        ArrayList<MsgItem> msgList = new ArrayList<>();
-
-//TODO        这段可以封装返回MsgItem的函数，image和text等用同一个函数，做成构造函数?
-        MsgItem item = new MsgItem();
-        item.setType("text");
-        Map<String, String> data  = new HashMap<>();
-        String resMsg = LocalChat.ChatByMsg(message);//
-        if (resMsg == null) {
-            resMsg = "出了点小问题...";
-        }
-        data.put("text",resMsg);
-        item.setData(data);
-
-        msgList.add(item);
-
-        MsgItem item1 = new MsgItem();
-        item1.setType("image");
-        Map<String, String> data1  = new HashMap<>();
-        data1.put("file","https://gchat.qpic.cn/gchatpic_new/1542338612/758025242-3108042462-E04E0D37BB15FF418729C4AE9118A180/0?term=255&is_origin=0");
-        item1.setData(data1);
-        msgList.add(item1);
-
-        params.put("message",msgList);
-        paramsRequest.setParams(params);
-
-        String msg = JSONObject.toJSONString(paramsRequest);
-        System.out.println(msg);
-        instance.session.getAsyncRemote().sendText(msg);
-    }
 
 }
 
